00f85e8191b13cf17fbdbc450392d779cd9796e9
offthewaugh <96805668+offthewaugh@users.noreply.github.com>
2023-01-22 22:56:29 -0500

    Add files via upload

---
+++ projectDetails.js
@@ -0,0 +1,73 @@
+let projectItems = [
+  {
+    id: "cloting store/shopping cart",
+    img: "https://www.shutterstock.com/shutterstock/photos/2104754999/display_1500/stock-vector-clothing-store-logo-design-inspiration-vector-illustration-2104754999.jpg",
+    description: `The clothing website is built with JavaScript using different
+    functions and methods. The website allows the user to select the
+    quantity of an item and then proceed to checkout with all items
+    selected.`,
+    development: ` 
+    <h3>Development</h3>
+    <li>JavaScript</li>
+    <li>HTML</li>
+    <li>CSS</li>
+    <li>Bootstrap</li>
+`,
+    developmentImage: `https://cdn-icons-png.flaticon.com/512/3067/3067260.png`,
+    sources: `              
+    <h3>Sources</h3>
+    <li>Youtube/freecodecamp</li>
+    <li>udemy</li>
+    <li>codecademy</li>
+    <li>w3schools</li>`,
+    sourcesImage: `https://www.equityinhighered.org/wp-content/uploads/2018/08/ACE_datasources_icon.svg"`,
+    webButton: "https://offthewaugh.github.io/clothingstore/index.html",
+    codeButton: "https://github.com/offthewaugh/clothingstore",
+  },
+  {
+    id: "Matching-Game",
+    img: "https://st.depositphotos.com/2885805/3842/v/600/depositphotos_38422667-stock-illustration-coming-soon-message-illuminated-with.jpg",
+    description: `The clothing website is built with JavaScript using different
+    functions and methods. The website allows the user to select the
+    quantity of an item and then proceed to checkout with all items
+    selected.`,
+    development: ` 
+    <h3>Development</h3>
+    <li>JavaScript</li>
+    <li>HTML</li>
+    <li>CSS</li>
+    <li>Bootstrap</li>
+`,
+    developmentImage: `https://cdn-icons-png.flaticon.com/512/3067/3067260.png`,
+    sources: `              
+    <h3>Sources</h3>
+    <li>Youtube/freecodecamp</li>
+    <li>udemy</li>
+    <li>codecademy</li>
+    <li>w3schools</li>`,
+    sourcesImage: `https://www.equityinhighered.org/wp-content/uploads/2018/08/ACE_datasources_icon.svg"`,
+  },
+  {
+    id: "FantasyFootballDraft",
+    img: "https://st.depositphotos.com/2885805/3842/v/600/depositphotos_38422667-stock-illustration-coming-soon-message-illuminated-with.jpg",
+    description: `The clothing website is built with JavaScript using different
+    functions and methods. The website allows the user to select the
+    quantity of an item and then proceed to checkout with all items
+    selected.`,
+    development: ` 
+    <h3>Development</h3>
+    <li>JavaScript</li>
+    <li>HTML</li>
+    <li>CSS</li>
+    <li>Bootstrap</li>
+`,
+    developmentImage: `https://cdn-icons-png.flaticon.com/512/3067/3067260.png`,
+    sources: `              
+    <h3>Sources</h3>
+    <li>Youtube/freecodecamp</li>
+    <li>udemy</li>
+    <li>codecademy</li>
+    <li>w3schools</li>`,
+    sourcesImage: `https://www.equityinhighered.org/wp-content/uploads/2018/08/ACE_datasources_icon.svg"`,
+  },
+];
