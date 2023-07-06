13bef01faa29438afb3c7c9c0ed0ee9a3db82da6
offthewaugh <96805668+offthewaugh@users.noreply.github.com>
2023-07-06 13:51:06 -0400

    Add files via upload

--- src/projectDetails.js
+++ src/projectDetails.js
@@ -25,48 +25,28 @@ let projectItems = [
     webButton: "https://offthewaugh.github.io/clothingstore/index.html",
     codeButton: "https://github.com/offthewaugh/clothingstore",
   },
-  //   {
-  //     id: "Matching-Game",
-  //     title: "Matching Game",
-  //     img: "https://st.depositphotos.com/2885805/3842/v/600/depositphotos_38422667-stock-illustration-coming-soon-message-illuminated-with.jpg",
-  //     description: `Match all the animals in order to win the game!`,
-  //     development: `
-  //     <h3>Development</h3>
-  //     <li>JavaScript</li>
-  //     <li>HTML</li>
-  //     <li>CSS</li>
-  //     <li>Bootstrap</li>
-  // `,
-  //     developmentImage: `https://cdn-icons-png.flaticon.com/512/3067/3067260.png`,
-  //     sources: `
-  //     <h3>Sources</h3>
-  //     <li>Youtube/freecodecamp</li>
-  //     <li>udemy</li>
-  //     <li>codecademy</li>
-  //     <li>w3schools</li>`,
-  //     sourcesImage: `https://www.equityinhighered.org/wp-content/uploads/2018/08/ACE_datasources_icon.svg"`,
-  //   },
-  //   {
-  //     id: "FantasyFootballDraft",
-  //     img: "https://st.depositphotos.com/2885805/3842/v/600/depositphotos_38422667-stock-illustration-coming-soon-message-illuminated-with.jpg",
-  //     description: `The clothing website is built with JavaScript using different
-  //     functions and methods. The website allows the user to select the
-  //     quantity of an item and then proceed to checkout with all items
-  //     selected.`,
-  //     development: `
-  //     <h3>Development</h3>
-  //     <li>JavaScript</li>
-  //     <li>HTML</li>
-  //     <li>CSS</li>
-  //     <li>Bootstrap</li>
-  // `,
-  //     developmentImage: `https://cdn-icons-png.flaticon.com/512/3067/3067260.png`,
-  //     sources: `
-  //     <h3>Sources</h3>
-  //     <li>Youtube/freecodecamp</li>
-  //     <li>udemy</li>
-  //     <li>codecademy</li>
-  //     <li>w3schools</li>`,
-  //     sourcesImage: `https://www.equityinhighered.org/wp-content/uploads/2018/08/ACE_datasources_icon.svg"`,
-  //   },
+  {
+    id: "Veyn-shopify",
+    title: "Veyn Custom Shopify Store",
+    img: "images/coming-soon.jpg",
+    description: `The clothing website is built with JavaScript using different
+    functions, methods, and api developer features. The website allows the user access to shopify's analytics features with a custom built website`,
+    development: ` 
+    <h3>Development</h3>
+    <li>JavaScript</li>
+    <li>HTML</li>
+    <li>CSS</li>
+    <li>Bootstrap</li>
+`,
+    developmentImage: `https://cdn-icons-png.flaticon.com/512/3067/3067260.png`,
+    sources: `              
+    <h3>Sources</h3>
+    <li>Youtube/freecodecamp</li>
+    <li>udemy</li>
+    <li>codecademy</li>
+    <li>w3schools</li>`,
+    sourcesImage: `https://www.equityinhighered.org/wp-content/uploads/2018/08/ACE_datasources_icon.svg"`,
+    webButton: "",
+    codeButton: "",
+  },
 ];
